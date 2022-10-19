12f707d0ae0a0fa47b641462c084555d7579e55a
waynee95 <larsericmarquardt@mail.de>
2022-10-19 18:46:13 +0200

    Add proof

--- Predic_Logic.thy
+++ Predic_Logic.thy
@@ -23,10 +23,26 @@ lemma "(\<forall>x. P(x) \<longrightarrow> Q) = ((\<exists>x. P(x)) \<longrighta
   apply (erule impE)
    apply (rule exI)
    apply assumption+
-   done
+  done
 
-lemma "\<forall>x. P(x)"
-  quickcheck
-  oops
-  
+lemma "(\<not> (\<forall>x. P x)) = (\<exists>x. \<not> P x)"
+proof (rule iffI)
+  assume "\<not> (\<forall>x. P x)"
+  show "\<exists>x. \<not> P x"
+  proof (rule ccontr)
+    assume "\<not> (\<exists>x. \<not> P x)"
+    then have "\<forall>x. P x" by simp 
+    with \<open>\<not> (\<forall>x. P x)\<close> show False by contradiction
+  qed
+next
+  assume "\<exists>x. \<not> P x"
+  then obtain c where "\<not> P c" by (rule exE)
+  show "\<not> (\<forall>x. P x)"
+  proof (rule notI)
+    assume "\<forall>x. P x"
+    then have "P c" by (rule allE)
+    with \<open>\<not> P c\<close> show False by contradiction
+  qed
+qed
+
 end
